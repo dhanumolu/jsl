101666b4b3e02140b495e229750e5f776f867f73
Subodh Hatkar <subodh.hatkar@gmail.com>
2020-06-30 11:45:03 +0100

    added entire pipeline

--- vars/entirePipeline.groovy
+++ vars/entirePipeline.groovy
@@ -18,6 +18,56 @@ def call() {
                     jacoco()
                 }
             }
+            stage('Sonarqube Analysis') {
+                steps {
+                    // Sonarqube Analysis
+                    withSonarQubeEnv('sonarqube') {
+                        sh '''
+                            mvn sonar:sonar -Dintegration-tests.skip=true
+                        '''
+                    }
+                    // Quality Gate Check
+                    withSonarQubeEnv('sonarqube') {
+                        timeout(time: 5, unit: 'MINUTES') {
+                            retry(3) {
+                                script {
+                                    def qg = waitForQualityGate()
+                                    if (qg.status != 'OK') {
+                                        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            stage('Docker Image Build') {
+                agent {
+                    kubernetes { label 'docker' }
+                }
+                steps {
+                    // Build Docker Image
+                    sh '''
+                        appName=$(basename `git config --get remote.origin.url` .git)
+                        appJar=$(find . -type f -path '**/target/**' -name '*SNAPSHOT.jar')
+                        jarName=$(basename $appJar .jar)
+                        dockerTag=${jarName#"$appName-"}
+                        gitCommitID=$(git log -n 1 --format="%h")
+                        docker build . \
+                            -t $appName:$dockerTag \
+                            --build-arg APP_JAR=$appJar \
+                            --build-arg GIT_COMMIT_ID=$gitCommitID \
+                            --build-arg JENKINS_BUILD_TAG=${BUILD_TAG}
+                        docker system prune -f
+                    '''
+                }
+            }
+        }
+        post {
+            always {
+                // Delete Workspace
+                deleteDir()
+            }
         }
     }
 }
